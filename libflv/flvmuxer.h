a5171d42f1391b731b23df686113999174555c8f
yxibng <yaoxiaobing@axzt.net>
2023-05-15 19:47:57 +0800

    feat: add muxer

---
+++ libflv/flvmuxer.h
@@ -0,0 +1,43 @@
+#ifndef __FLVMUXER_H__
+#define __FLVMUXER_H__
+
+#include <iostream>
+
+namespace nx
+{
+
+
+
+
+
+class FlvMuxer {
+
+private:
+
+bool hasVideo;
+bool hasAudio;
+FILE *flvFile;
+
+public:
+
+
+
+
+
+
+
+};
+
+
+    
+} // namespace nx
+
+
+
+
+
+
+
+
+
+#endif // __FLVMUXER_H__
